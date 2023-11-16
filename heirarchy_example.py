0de54a193360f7ab3cbf7c52b24d8ccccd253343
Jeffrey Jeyachandren <jeffreyjeyachandren@Jeffreys-Mac-mini.local>
2023-11-15 21:56:40 -0500

    roles pdf generating

--- heirarchy_example.py
+++ heirarchy_example.py
@@ -2,12 +2,29 @@ import autogen
 from autogen.agentchat.groupchat import GroupChat
 from autogen.agentchat.agent import Agent
 from autogen.agentchat.assistant_agent import AssistantAgent
+from pdf_reports import pug_to_html, write_report
+import sys
+
 config_list = autogen.config_list_from_json(
     env_or_file="OAI_CONFIG_LIST",
     filter_dict={
         "model": ["gpt-4-1106-preview"],
     },
 )
+
+class DualOutput:
+    def __init__(self, filename):
+        self.terminal = sys.stdout
+        self.log = open(filename, "w")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        # This flush method is needed for compatibility with the standard output.
+        self.terminal.flush()
+        self.log.flush()
 
 llm_config = {"config_list": config_list}
 
@@ -71,7 +88,7 @@ def is_termination_msg(content) -> bool:
 
         return True
     return False
-
+"""
 def generate_pug_table_rows(details):
     rows = []
     for role, messages in details.items():
@@ -80,6 +97,33 @@ def generate_pug_table_rows(details):
         row = f"<tr><td>{role}</td><td>{last_message}</td></tr>"
         rows.append(row)
     return "\n".join(rows)
+"""
+
+def generate_pug_table_rows(data):
+    pug_rows = []
+    pug_rows.append("\n")
+    
+    for role, messages in data.items():
+        # messages are in a list and the last message is most recent
+        last_message = messages[-1] if messages else "No feedback"
+
+        # Split the last_message into lines
+        message_lines = last_message.split('\n')
+
+        # Start a new row
+        pug_row = "    tr\n"
+
+        # Add the role to the row
+        pug_row += f"      td {role}\n"
+
+        # Add the message to the row, handling multiple lines
+        pug_row += "      td\n"
+        for line in message_lines:
+            pug_row += f"        | {line}\n"
+
+        pug_rows.append(pug_row)
+    
+    return "\n".join(pug_rows)
 
 def exec_python(results):
 
@@ -104,42 +148,54 @@ def exec_python(results):
         {{ important_message_body }}
 
 
-    :markdown
-      ## Results
-    table.ui.celled.table
-      thead
-        tr
-          th Role
-          th Last Message
-      tbody
-        {{ table_rows }}
+:markdown
+    ## Results
+table.ui.celled.table
+  thead
+    tr
+      th Role
+      th Analysis
+  tbody
+      {{ table_rows }}
 
-    :markdown
-      ## Appendix
-      ### Team Leaders Weigh In
-      p.
-        {{ team_leader_last_message }}
-
-    """
-
-    s = cell.strip('"\'')
-    while s.startswith('#'):
-        s = s[1:].strip()
+:markdown
+    ## Appendix
+    ### Team Leaders Weigh In
+    p.
+    waddup
     
-    exec_summ_prefix = 'Executive Summary:'
-    if s.startswith(exec_summ_prefix):
-        s = s[len(exec_summ_prefix):].strip()
 
-    important_message_body = s
-    table_rows = generate_pug_table_rows(details)
-    pug_with_table = pug_template_string.replace("{{ table_rows }}", table_rows).replace("{{ team_leader_last_message }}", team_leader_last_message)
+"""
+
+
+    # team_members = "Team A: A2 (engineer), A3 (architect). Team B: B2 (compliance officer), B3 (business stakeholder), and B4 (Threat Modeler)."
+    important_message_body = """This report outlines the key findings and recommendations from a threat modeling exercise focused on a given application architecture (see appendix), with contributions from two distinct teams.
+
+Team Composition and Perspectives:
+
+Team A: Comprising A2 (Engineer) and A3 (Architect), focused on technical vulnerabilities and architectural improvements.
+Team B: Including B2 (Compliance Officer), B3 (Business Stakeholder), and B4 (Threat Modeler), emphasizing compliance, business impact, and holistic threat modeling. 
+
+Key outcomes of the exercise include:
+
+Identification of Critical Threats: The team pinpoints several high-priority threats that could significantly impact the application's security. These include risks related to data breaches, unauthorized access, and system downtimes.
+
+Mitigation Strategies: For each identified threat, the team proposes tailored mitigation strategies. These range from immediate short-term fixes to long-term structural changes in the application's architecture.
+
+Prioritization of Actions: The team has collectively prioritized the proposed actions based on the severity of threats and the feasibility of implementing solutions. This prioritization aims to optimize resource allocation and ensure a rapid response to the most critical issues.
+
+"""
+    table_rows = generate_pug_table_rows(results)
+    print("\n\ntable rows\n\n", table_rows)
+    pug_with_table = pug_template_string.replace("{{ table_rows }}", table_rows)
+
+    print("\n\npug with table\n\n", pug_with_table)
 
     html = pug_to_html(string=pug_with_table, 
-                       important_message_body=important_message_body,
-                       longform=longform)
+                       important_message_body=important_message_body)
 
     # Generate the report
-    write_report(html, "stride_report.pdf")
+    write_report(html, "stakeholder_report.pdf")
     return "Report generated successfully"
 
 
@@ -151,7 +207,8 @@ def parse_report_details(messages):
     "B1": "Team B Leader",
     "B2": "Compliance Officer",
     "B3": "Business Stakeholder",
-    "B4": "Threat Modeler"
+    "B4": "Threat Modeler",
+    "User_proxy": "Human User"
     }
     
     results = {}
@@ -170,11 +227,12 @@ def parse_report_details(messages):
                         report_details = message['content']
                         results.setdefault(role, []).append(report_details)
 
+    #print("PUG table results", results)
     return results
 # Initialization
 agents_A = [
     AssistantAgent(name='A1', 
-                   system_message="You are a team leader A1, your team consists of A2 (engineer), A3 (architect). You can talk to the other team leader B1, whose team members are B2 (compliance officer) and B3 (business stakeholder). Make sure your team members give their analysis of the report results.",
+                   system_message="You are a team leader A1, your team consists of A2 (engineer), A3 (architect). You can talk to the other team leader B1, whose team members are B2 (compliance officer) and B3 (business stakeholder). Make sure your team members give their analysis of the report results, but only after B1 has provided you with their STRIDE/DREAD analysis. If you have not heard from B1 yet, use 'NEXT: B1' to suggest talking to B1.",
                    llm_config=llm_config),
     AssistantAgent(name='A2', 
                    system_message="You are team member A2 (engineer). To cooperate, you tell others the potential coding pitfalls in the app architecture that might lead to vulnerabilities and suggest possible solutions. After reviewing the STRIDE and DREAD results of the app architecture, you provide a formalized analysis of the report results which is related to implementation details and mitigation strategies based on coding best practices. You must start your write-up for the report with 'Engineering Discussion:'.",
@@ -186,7 +244,7 @@ agents_A = [
 
 agents_B = [
     AssistantAgent(name='B1', 
-                   system_message="You are a team leader B1. Your team consists of B2 (compliance_officer), B3 (business_stakeholder), and B4 (Threat Modeler). You can talk to the other team leader A1, whose team members are A2 (engineer) and A3 (architect). Use NEXT: A1 to suggest talking to A1. You should suggest that B4 (Threat Modeler) start the analysis.",
+                   system_message="You are a team leader B1. Your team consists of B2 (compliance_officer), B3 (business_stakeholder), and B4 (Threat Modeler). You can talk to the other team leader A1, whose team members are A2 (engineer) and A3 (architect). If you have not heard from A1 yet, use 'NEXT: A1' to suggest talking to A1 and send them the STRIDE/DREAD analysis of the app architecture. When A1 confirms that their team members have provided feedback, you can tell B4 ('NEXT: B4') the analysis is done and to append a new line with TERMINATE to the report.",
                    llm_config=llm_config),
     AssistantAgent(name='B2', 
                    system_message="You are team member B2 (compliance_officer). To cooperate, you tell others whether the identified threats and their mitigation would put the organization at risk of non-compliance. After reviewing the STRIDE and DREAD results of the app architecture, you provide a formalized analysis of the report results which is related to how the threats and mitigations align with compliance standards and regulations. You must start your write-up for the report with 'Compliance Officer Discussion:'.",
@@ -195,7 +253,7 @@ agents_B = [
                    system_message="You are team member B3 (business_stakeholder). To cooperate, you tell others the business context. After reviewing the STRIDE and DREAD results of the app architecture, you provide a formalized analysis of the report results which is related to explaining the critical assets, business impact of potential threats, and which mitigations to prioritize based on business needs. You must start your write-up for the report with 'Business Stakeholder Discussion:'.",
                    llm_config=llm_config),
     AssistantAgent(name='B4', 
-                   system_message="You are team member B4. Your task is to identify the components and attack vectors in the app architecture, and do a STRIDE and DREAD analysis on each component/threat. Make sure your STRIDE/DREAD analysis starts with 'STRIDE/DREAD Analysis:'. Once you have done that, you get feedback from the A2 (engineer), A3 (architect), B2 (compliance officer), and B3 (business stakeholder). Once everyone has provided feedback, append a new line with TERMINATE.",
+                   system_message="You are team member B4. Wait for B1 to confirm the analysis is done, and then append a new line with TERMINATE.",
                    llm_config=llm_config)
 ]
 
@@ -205,7 +263,7 @@ user_proxy = autogen.UserProxyAgent(
         system_message="Terminator admin.",
         code_execution_config=False,
         is_termination_msg=is_termination_msg,
-        human_input_mode="NEVER")
+        human_input_mode="ALWAYS")
 
 list_of_agents = agents_A + agents_B
 list_of_agents.append(user_proxy)
@@ -213,17 +271,21 @@ list_of_agents.append(user_proxy)
 # Create CustomGroupChat
 group_chat = CustomGroupChat(
     agents=list_of_agents,  # Include all agents
-    messages=['Everyone cooperates and help B4 in their task. Team A has A1, A2 (engineer), A3 (architect). Team B has B1, B2 (compliance officer), and B3 (business stakeholder), and B4. Only members of the same team can talk to one another. Only team leaders (names ending with 1) can talk amongst themselves. You must use "NEXT: B1" to suggest talking to B1 for example; You can suggest only one person, you cannot suggest yourself or the previous speaker; You can also dont suggest anyone.'],
+    messages=['Everyone cooperates and help B4 in their task. Team A has A1, A2 (engineer), A3 (architect). Team B has B1, B2 (compliance officer), and B3 (business stakeholder), and B4. Only members of the same team can talk to one another. Only team leaders (names ending with 1) can talk amongst themselves. You must use "NEXT: B1" to suggest talking to B1 for example; You can suggest only one person, you cannot suggest yourself or the previous speaker. Team leaders can identify the components and attack vectors in the app architecture, and do an analysis of each identified component/vector using STRIDE and DREAD - which they provide to their team.'],
     max_round=30
 )
 
 
 # Create the manager
-llm_config = {"config_list": config_list, "cache_seed": 22}  # cache_seed is None because we want to observe if there is any communication pattern difference if we reran the group chat.
+llm_config = {"config_list": config_list, "seed": 28}  # cache_seed is None because we want to observe if there is any communication pattern difference if we reran the group chat.
 
 manager = autogen.GroupChatManager(groupchat=group_chat, llm_config=llm_config)
 
 app_architecture = "The application architecture is a web application with a database. The web application is written in Python and uses the Flask framework. The database is a MySQL database. The web application is hosted on AWS EC2. The web application is a simple blog application that allows users to create posts and comment on posts. The web application uses a MySQL database to store the posts and comments. The web application uses the Flask framework to handle requests and responses. The web application uses the Jinja2 templating engine to render HTML templates. The web application uses the WTForms library to handle forms. The web application uses the Flask-Login library to handle user authentication. The web application uses the Flask-WTF library to handle forms. The web application uses the Flask-Bootstrap library to handle forms. The web application uses the Flask-Admin library to handle forms. The web application uses the Flask-RESTful library to handle forms."
-message = "Identify the components and attack vectors in this app architecture, and then get an analysis of each identified component/vector using STRIDE and DREAD. Detailed feedback must be provided from the perspective of the engineer, architect, business stakeholder, and compliance officer. App architecture: " + app_architecture
-# Initiates the chat with B2
+message = "Identify the components and attack vectors in this app architecture, and then get an analysis of each identified component/vector using STRIDE and DREAD. App architecture: " + app_architecture
+
+
+sys.stdout = DualOutput('conversation.log')
 agents_B[0].initiate_chat(manager, message=message)
+sys.stdout.log.close()
+sys.stdout = sys.stdout.terminal
